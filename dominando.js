43508e0c48f56e6786b3c93f889b1a1060a2ffe7
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-05 13:58:54 -0300

    Em teoria Funciona

--- dominando.js
+++ dominando.js
@@ -1,28 +1,31 @@
 const transpose = (matrix) => {
     let [row] = matrix
     return row.map((value, column) => matrix.map(row => row[column]))
-  }
+}
 function TTTChecker(array) {
     let estaOcupadoPor =
-     {
-        vazio : 0,
-        bolinha : 2,
-        xizinho : 1,
+    {
+        vazio: 0,
+        bolinha: 2,
+        xizinho: 1,
     }
     let tipoDoResultadoEh =
     {
-        naoAcabado : -1,
-        xizinhoWins : 1,
-        bolinhaWins : 2,
-        empatou : 0
+        naoAcabado: -1,
+        xizinhoWins: 1,
+        bolinhaWins: 2,
+        empatou: 0
     }
-    
+
     function TresCasasSaoIguais(arrayDeTresPosicoes) {
         return (arrayDeTresPosicoes[0] == arrayDeTresPosicoes[1] && arrayDeTresPosicoes[1] == arrayDeTresPosicoes[2])
     }
 
     function ArrayChecker(arrayDeTresPosicoes) {
-        
+        if (arrayDeTresPosicoes.includes(0)) {
+            return tipoDoResultadoEh.naoAcabado
+        }
+
         if (TresCasasSaoIguais(arrayDeTresPosicoes)) {
             let elementoQueEhIgual = arrayDeTresPosicoes[0]
 
@@ -32,11 +35,9 @@ function TTTChecker(array) {
             if (elementoQueEhIgual == estaOcupadoPor.bolinha) {
                 return tipoDoResultadoEh.bolinhaWins
             }
-          
+
         }
-        if (arrayDeTresPosicoes.includes(0)){
-            return tipoDoResultadoEh.naoAcabado
-        }
+
         else {
             return tipoDoResultadoEh.empatou
         }
@@ -53,17 +54,32 @@ function TTTChecker(array) {
         return 0
     }
 
-  let resultadoLinhas = RowsChecker(array)
-  let resultadoColunas = RowsChecker(transpose(array))
-return resultadoColunas
-    
+    let resultadoLinhas = RowsChecker(array)
+    let resultadoColunas = RowsChecker(transpose(array))
+
+    let diagonalUm = [array[0][0], array[1][1], array[2][2]]
+    let diagonalDois = [array[2][0], array[1][1], [0][2]]
+    let resultadoDiagonalUm = ArrayChecker(diagonalUm)
+    let resultadoDiagonalDois = ArrayChecker(diagonalDois)
+
+    const ProvaFinal = () => {
+        const tipoDoResultado_ = [2, 1, -1, 0]
+        for (let tipoDoResultado of tipoDoResultado_) {
+
+            if (resultadoLinhas == tipoDoResultado || resultadoColunas == tipoDoResultado || resultadoDiagonalUm == tipoDoResultado || resultadoDiagonalDois == tipoDoResultado) {
+                return tipoDoResultado
+
+            }
+        }
+    }
+    return ProvaFinal()
 };
 
 
 let arrayTeste =
-   [[1, 1, 1],
-    [2, 0, 2],
-    [2, 0, 1]]
+    [[1, 2, 2],
+    [2, 1, 1],
+    [2, 1, 2]]
 
 console.log(TTTChecker(arrayTeste))
                  /*
